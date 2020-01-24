ee4dfa13a481923a421b5588e369334db621b368
Mark Vincent <markdeanilvicente@gmail.com>
2020-01-24 11:51:32 +0800

    update index

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-function randomName(country, gender, nameType) {
-  var fs = require("fs");
-  var obj;
+import * as fs from "fs";
 
-  fs.readFile(`by-country/${country}/${gender}/${nameType}.json`, "utf8", function(err, data) {
-    if (err) throw err;
-    obj = JSON.parse(data);
-    const values = Object.values(obj);
-    const randomValue = values[parseInt(Math.random() * values.length)];
-    return randomValue.Name;
-  });
+function randomName(country, gender, nameType) {
+  var obj;
+  fs.readFile(
+    `by-country/${country}/${gender}/${nameType}.json`,
+    "utf8",
+    function(err, data) {
+      if (err) throw err;
+      obj = JSON.parse(data);
+      const values = Object.values(obj);
+      const randomValue = values[parseInt(Math.random() * values.length)];
+      return randomValue.Name;
+    }
+  );
 }
 
 module.exports.randomName = randomName;
