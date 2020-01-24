8c3464ff817f7e561c9ff5021265a057b9b33b21
Mark Vincent <markdeanilvicente@gmail.com>
2020-01-24 12:17:00 +0800

    add jsonfilepackage

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-import * as fs from "fs";
-
 function randomName(country, gender, nameType) {
-  var obj;
-  fs.readFile(
-    `by-country/${country}/${gender}/${nameType}.json`,
-    "utf8",
-    function(err, data) {
-      if (err) throw err;
-      obj = JSON.parse(data);
-      const values = Object.values(obj);
-      const randomValue = values[parseInt(Math.random() * values.length)];
-      return randomValue.Name;
-    }
-  );
+  const jsonfile = require("jsonfile");
+  const file = `by-country/${country}/${gender}/${nameType}.json`;
+
+  jsonfile.readFile(file, function(err, obj) {
+    if (err) console.error(err);
+    const values = Object.values(obj);
+    const randomValue = values[parseInt(Math.random() * values.length)];
+    return randomValue.Name;
+  });
 }
 
 module.exports.randomName = randomName;
